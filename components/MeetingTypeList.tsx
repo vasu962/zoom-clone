a3a296bacda17fc0c89344095cd6de5265b8c37c
Vasu <vasukumar962@gmail.com>
2024-04-04 13:01:02 +0530

    added reusable model

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -3,6 +3,7 @@
 import { useState } from 'react';
 import HomeCard from './HomeCard';
 import { useRouter } from 'next/navigation';
+import MeetingModel from './MeetingModel';
 
 const MeetingTypeList = () => {
   const router = useRouter();
@@ -10,13 +11,15 @@ const MeetingTypeList = () => {
     'isSchedulMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined
   >();
 
+  const createMeeting = () => {};
+
   return (
     <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-5">
       <HomeCard
         img="/icons/add-meeting.svg"
         title="New Meeting"
         description="Start an instant meeting"
-        handleClick={() => setMeetingState('isJoiningMeeting')}
+        handleClick={() => setMeetingState('isInstantMeeting')}
         className="bg-orange-1"
       />
       <HomeCard
@@ -39,6 +42,15 @@ const MeetingTypeList = () => {
         description="via invitation link"
         handleClick={() => setMeetingState('isJoiningMeeting')}
         className="bg-yellow-1"
+      />
+
+      <MeetingModel
+        isOpen={meetingState === 'isInstantMeeting'}
+        onClose={() => setMeetingState(undefined)}
+        title="Start an Instant Meeting"
+        className="text-center"
+        buttonText="Start Meeting"
+        handleClick={createMeeting}
       />
     </section>
   );
